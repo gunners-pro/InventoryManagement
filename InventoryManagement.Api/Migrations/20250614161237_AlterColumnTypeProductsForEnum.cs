5ba44a273b921523b01c1f1351961580d68f2732
Fabricyo Barreto <bass.barreto@gmail.com>
2025-06-14 13:43:16 -0300

    chore: add altercolumnProduct migration

---
+++ InventoryManagement.Api/Migrations/20250614161237_AlterColumnTypeProductsForEnum.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace InventoryManagement.Api.Migrations
+{
+    /// <inheritdoc />
+    public partial class AlterColumnTypeProductsForEnum : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("CREATE TYPE product_category AS ENUM ('Clothing', 'Shoes', 'Bag', 'Jewelry', 'Acessories');");
+            migrationBuilder.Sql("CREATE TYPE product_status AS ENUM('Active', 'OutOfStock')");
+
+            migrationBuilder.Sql("ALTER TABLE products DROP COLUMN \"Category\";");
+            migrationBuilder.Sql("ALTER TABLE products ADD COLUMN \"Category\" product_category NOT NULL;");
+            migrationBuilder.Sql("ALTER TABLE products DROP COLUMN \"Status\";");
+            migrationBuilder.Sql("ALTER TABLE products ADD COLUMN \"Status\" product_status NOT NULL;");
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("ALTER TABLE products ALTER COLUMN \"Category\" TYPE int;");
+            migrationBuilder.Sql("ALTER TABLE products ALTER COLUMN \"Status\" TYPE int;");
+
+            migrationBuilder.Sql("DROP TYPE product_category;");
+            migrationBuilder.Sql("DROP TYPE product_status;");
+        }
+    }
+}
